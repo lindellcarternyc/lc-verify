2fac0741f5b21e12dd10ff425abbd52ae510ae67
Lindell Carter <lindellcarternyc@gmail.com>
2020-02-25 15:56:52 -0500

    add object error types

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,3 +1,11 @@
 export const primitiveError = (tag: string) => (value: any) => {
   return `${JSON.stringify(value)} is not a valid ${tag}`
 }
+
+export const objectError = (validatorName: string) => (key: string) => (error: string): string => {
+  return `<${validatorName}> validator failed at key "${key}" with error: ${error}`
+}
+
+export const objectMappedKeyError = (validatorName: string) => (key: string) => (mappedKey: string) => (error: string): string => {
+  return `<${validatorName}> validator failed at key "${key}" (mapped from object key "${mappedKey}") with error: ${error}`
+}
